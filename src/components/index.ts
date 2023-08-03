a0fbe24cf09f70ae5a698a158a4a3f172315b761
Naveen-McDev <naveenmcdeveloper@gmail.com>
2023-08-03 22:14:43 +0530

    Navbar and footer done

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 import CustomButton from "./CustomButton";
+import Footer from "./Footer";
 import Hero from "./Hero";
+import NavBar from "./Navbar";
 
-export { Hero, CustomButton };
+export { Hero, CustomButton, NavBar, Footer };
